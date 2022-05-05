079d9a6b99c86b05c15f6c4498bdefbfbd431220
Obong-B <ooollollollo@gmail.com>
2022-05-05 17:05:42 +0900

    JS : transparent

--- main.js
+++ main.js
@@ -13,10 +13,8 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the nabvar menu
 const navbarMenu = document.querySelector(".navbar__memu");
-
 navbarMenu.addEventListener('click', (event) => {
     const link = event.target.dataset.link;
-
     if(link == null){
         return;
     }
@@ -24,12 +22,19 @@ navbarMenu.addEventListener('click', (event) => {
 });
 
 // Handle click on "contact me" Button on home
-
 const homeContactBtn = document.querySelector(".home__contact");
-
 homeContactBtn.addEventListener('click', (event) => {
     scrollIntoView('#contact');
 });
+
+//Make hoem slowly fade to transparent as the window scroll down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1- window.scrollY / homeHeight;
+});
+
+
 
 
 function scrollIntoView(selector){
